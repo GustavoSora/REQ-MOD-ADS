81ac14ee32458467662dcf4234cf9154cb7415f0
Gustavo Sora <151575299+GustavoSora@users.noreply.github.com>
2024-09-19 20:23:58 -0300

    Add files via upload
    exercício da aula, transformar o diagrama em código

---
+++ projeto-aula-conjunta-POOrmas/src/Faculdade/Estudante.java
@@ -0,0 +1,17 @@
+package Faculdade;
+
+public class Estudante extends Pessoa {
+
+    private String matricula;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
+
+
+
